5a8dec8b5ec820906e626c5d4b1272b585554897
artemy <petra.poorino@gmail.com>
2021-10-07 02:30:58 +0300

    Роутизация

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/msk')
+def index_msk():
+    return render_template('index.html', title = 'Москва')
 
+
+@app.route('/vol')
+def index_vol():
+    return render_template('index.html', title = 'Вологда')
+
+
+@app.route('/spb')
+def index_spb():
+    return render_template('index.html', title = 'Санкт-Петербург')
+
+
+@app.route('/kns')
+def index_kns():
+    return render_template('index.html', title = 'Краснодар')
 
 if __name__ == '__main__':
     app.run()
