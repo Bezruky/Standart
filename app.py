d17e6089b245a68820df9256ee66995798a07839
artemy <petra.poorino@gmail.com>
2021-10-08 02:32:09 +0300

    Создание БД, ORM моделей и заполнение таблиц

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+
+from models import Cities, Products, Reviews
+
+# cities = Cities.query.all()
+# for p in cities:
+#     print(p.id, p.author.username, p.body)
 
 
 @app.route('/')
@@ -22,6 +32,10 @@ def index_spb():
 @app.route('/kns')
 def index_kns():
     return render_template('index.html', title = 'Краснодар')
+
+@app.errorhandler(404)
+def pageNotFound(error):
+    return render_template('base.html')
 
 if __name__ == '__main__':
     app.run()
---
+++ models.py
@@ -0,0 +1,34 @@
+from app import db
+
+
+class Cities(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    city_name = db.Column(db.String(50))
+    tel_num = db.Column(db.String(15))
+    coords = db.Column(db.String(50))
+    flag = db.Column(db.Boolean)
+
+    def __repr__(self):
+        return '<Cities {}>'.format(self.city_name)
+
+
+class Products(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    product_name = db.Column(db.String(150))
+    price = db.Column(db.Integer)
+    quantity = db.Column(db.Integer)
+    flag = db.Column(db.Boolean)
+
+    def __repr__(self):
+        return '<Products {}>'.format(self.product_name)
+
+
+class Reviews(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    author_name = db.Column(db.String(150))
+    review_text = db.Column(db.Text)
+    photo = db.Column(db.String(70))
+    flag = db.Column(db.Boolean)
+
+    def __repr__(self):
+        return '<Reviews {}>'.format(self.author_name)
